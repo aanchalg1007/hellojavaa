b0f74d9c768515bb24136f326bbd9b6370896189
aanchalg1007 <107946012+aanchalg1007@users.noreply.github.com>
2026-03-08 18:20:25 +0530

    Update hellojava.java

--- hellojava.java
+++ hellojava.java
@@ -1,4 +1,4 @@
-public class hello{
+public class hellojava{
     public static void main(String[]args){
         System.out.println("Hello SIESCOMS");
         System.out.println("This is Jenkins Project");
