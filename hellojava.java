3b81d9867f11f007049c2e0dca3564dd497ebeb1
aanchalg1007 <107946012+aanchalg1007@users.noreply.github.com>
2026-03-08 20:50:15 +0530

    Update hellojava.java

--- hellojava.java
+++ hellojava.java
@@ -2,6 +2,6 @@ public class hellojava{
     public static void main(String[]args){
         System.out.println("Hello SIESCOMS");
         System.out.println("This is Jenkins Project");
-         System.out.println("Addition :"+ (5 + 3));
+         System.out.println("Addition :"+ (8 + 2));
     }
 }
